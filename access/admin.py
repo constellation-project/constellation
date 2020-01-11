5f0b406b73dd59412a399a7f1241243048865441
Benjamin Graillot <graillot@crans.org>
2020-01-11 16:04:14 +0100

    [access] Admin interface

--- access/admin.py
+++ access/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Switch, Port
+
+
+class PortInline(admin.TabularInline):
+    extra = 0
+    model = Port
+
+@admin.register(Switch)
+class SwitchAdmin(admin.ModelAdmin):
+    list_display = ('name', 'model', 'building', 'location')
+    inlines = (PortInline,)
--- access/models.py
+++ access/models.py
@@ -16,14 +16,17 @@ class Switch(Machine):
     location = models.CharField(max_length=255)
     model = models.CharField(max_length=32)
 
-class SwitchPorts(models.Model):
+class Port(models.Model):
     switch = models.ForeignKey(Switch, on_delete=models.CASCADE, related_name='ports')
-    module = models.CharField(max_length=16, null=True)
+    module = models.CharField(max_length=16, blank=True)
     number = models.PositiveSmallIntegerField()
     vlans = models.ManyToManyField('layers.Vlan')
-    room = models.ForeignKey('topography.Room', null=True, on_delete=models.SET_NULL, related_name='switch_ports')
-    port = models.ForeignKey('self', null=True, on_delete=models.SET_NULL, related_name='+')
-    interface = models.ForeignKey('layers.Interface', null=True, on_delete=models.SET_NULL, related_name='switch_port')
+    room = models.ForeignKey('topography.Room', null=True, blank=True, on_delete=models.SET_NULL, related_name='switch_ports')
+    port = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
+    interface = models.ForeignKey('layers.Interface', null=True, blank=True, on_delete=models.SET_NULL, related_name='switch_port')
+
+    def __str__(self):
+        return f"{self.module}{self.number}@{self.switch.name}"
 
     class Meta:
         unique_together = ('switch', 'module', 'number')
